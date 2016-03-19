9a78264c91ddd2bc077a2914e91cf3a4a74dd865
CJ <franciscarlton@yahoo.com>
2016-03-19 15:29:48 -0400

    Reformatted map.js to be cleaner and work closer to final behavior, also made google maps api call synchronous

--- src/js/controllers/map.js
+++ src/js/controllers/map.js
@@ -1,37 +1,62 @@
 "use strict";
 angular.module('sparFinder')
-    .controller('mapCTRL', ["$scope", "dataService", function ($scope, dataService) {
+    .controller('mapCTRL', ["$scope", "$timeout", "dataService", function ($scope, $timeout, dataService) {
 
-        $scope.createMarkers = function (users) {
-            for (var i = 0; i < users.length; i++) {
-                new google.maps.Marker({
-                    map: $scope.map,
-                    position: {
-                        lat: users[i]['lat'],
-                        lng: users[i]['lng']
-                    }
-                });
-            }
-        };
+        $scope.initMap = function () {
 
-        window.initMap = function () {
-            dataService.getUsers(function (response) {
-                $scope.users = response.data.users;
-                $scope.centerLatLng = {
-                    lat: $scope.users[0]['lat'],
-                    lng: $scope.users[0]['lng']
-                };
+            var locationUSA = {
+                "lat": 37.09024,
+                "lng": -95.712891
+            };
 
-                // Create a map object and specify the DOM element for display.
-                $scope.map = new google.maps.Map(document.getElementById('map'), {
-                    center: $scope.centerLatLng,
-                    scrollwheel: false,
-                    zoom: 10
-                });
-
-                // Create markers and set they're position.
-                $scope.createMarkers($scope.users);
+            $scope.map = new google.maps.Map(document.getElementById('map'), {
+                center: locationUSA,
+                scrollwheel: false,
+                zoom: 3
             });
         };
+
+        $scope.createMarkers = function (users) {
+            var markers = [];
+            for (var i = 0; i < users.length; i++) {
+                markers.push(
+                    new google.maps.Marker({
+                        map: $scope.map,
+                        position: {
+                            lat: users[i]['lat'],
+                            lng: users[i]['lng']
+                        },
+                        title: users[i]['first_name']+' '+users[i]['last_name']
+                    })
+                );
+            }
+            return markers;
+        };
+
+        $scope.reFocusMap = function (markers) {
+            var bounds = new google.maps.LatLngBounds();
+            for (var i = 0; i < markers.length; i++) {
+                bounds.extend(markers[i].getPosition());
+            }
+            $scope.map.fitBounds(bounds);
+            $scope.map.setZoom(10);
+        };
+
+        $scope.updateMap = function (users) {
+            $scope.reFocusMap($scope.createMarkers(users));
+        };
+
+        $scope.initMap();
+
+        //Delete when actual functionality is implemented
+
+        $scope.simulateQuery = function () {
+            dataService.getUsers(function (response) {
+                var users = response.data.users;
+                $scope.updateMap(users);
+            });
+        };
+
+        $timeout(3000).then($scope.simulateQuery);
 
     }]);
